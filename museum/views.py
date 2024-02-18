69e13b24c3450fa87dbd99d8c61ef8d123d1acdd
Pavlo <pavlosenchak14@gmail.com>
2024-02-18 14:11:19 +0200

    changed "index"

--- museum/views.py
+++ museum/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello world!@')
+    return HttpResponse('Hello world!@ test')
 def index(request):
     return HttpResponse('lol@')
